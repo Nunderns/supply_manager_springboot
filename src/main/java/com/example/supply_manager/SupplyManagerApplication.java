ad83c33b8bcd0b7fb5eee16f5725a8db9b82aa00
Henri Okayama <henri.okayama@gmail.com>
2025-09-29 10:51:21 -0300

    primeiro commit

---
+++ src/main/java/com/example/supply_manager/SupplyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.supply_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplyManagerApplication.class, args);
+	}
+
+}
